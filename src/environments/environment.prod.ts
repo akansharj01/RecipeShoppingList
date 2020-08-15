e738e1403a557864b12c52b9e0f0fcd79a8b8772
akansharj01 <59988885+akansharj01@users.noreply.github.com>
2020-08-15 11:55:13 +0530

    revert changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  fireBaseKey: 'AIzaSyDWlqNfPrtwTv5xDU9_sk_bMN8gG5R4d0s'
+  fireBaseKey: 'abc'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  fireBaseKey: 'AIzaSyDWlqNfPrtwTv5xDU9_sk_bMN8gG5R4d0s'
+  fireBaseKey: 'abc'
 };
 
 /*
